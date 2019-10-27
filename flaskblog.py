8281280404dc5087eef65942f0981f2444e7d868
peninah-njeri <peninahgathuru@gmail.com>
2019-10-27 21:38:57 +0300

    install sqlalchemy and set up  database

--- flaskblog.py
+++ flaskblog.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template,url_for,flash,redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
 app=Flask(__name__)
 
 
 app.config['SECRET_KEY']= 'efce622448d3b2f542f80b81715738ad'
+app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///site.db'
 
+db=SQLAlchemy(app)
 
 posts = [
     {
@@ -39,13 +42,16 @@ def register():
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
-@app.route('/login')
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    form=RegistrationForm()
-    render_template('login.html', title='register',form=form)
-
-
-
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 
 
