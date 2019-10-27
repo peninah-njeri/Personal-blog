a6748119a92409e2a3accb635d931bac99e4e09f
peninah-njeri <peninahgathuru@gmail.com>
2019-10-27 19:45:23 +0300

    add a list of allowed methods in a the register route

--- flaskblog.py
+++ flaskblog.py
@@ -31,10 +31,13 @@ def home():
 def about():
     return render_template('about.html',title='About')
 
-@app.route('/register')
+@app.route("/register", methods=['GET', 'POST'])
 def register():
-    form=RegistrationForm()
-    render_template('register.html', title='register',form=form)
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
 
 @app.route('/login')
 def login():
