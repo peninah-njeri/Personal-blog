e484aaee2f70f36de0d2b0360d072168c6649b71
peninah-njeri <peninahgathuru@gmail.com>
2019-10-29 21:09:24 +0300

    add user account route, where a user can add account details and also upload a profile pic

--- app/__init__.py
+++ app/__init__.py
@@ -9,5 +9,8 @@ app.config['SECRET_KEY']= 'efce622448d3b2f542f80b81715738ad'
 app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///site.db'
 db=SQLAlchemy(app)
 bcrypt=Bcrypt(app)
-from app import routes
 login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+from app import routes
+
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 from flask import render_template,url_for,flash,redirect
-from app.forms import RegistrationForm, LoginForm
+from app.forms import RegistrationForm, LoginForm,UpdateAccountForm
 from app import app,db,bcrypt
 from app.models import User
+from flask_login import login_user, current_user, logout_user,login_required
 
 posts = [
     {
@@ -29,6 +30,8 @@ def about():
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password= bcrypt.generate_password_hash(form.password.data).decode ("__utf-8")
@@ -41,12 +44,29 @@ def register():
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash('You have been logged in!', 'success')
-            return redirect(url_for('home'))
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password,form.password.data):
+           login_user(user)
+           return redirect(url_for('home'))
         else:
-            flash('Login Unsuccessful. Please check username and password', 'danger')
+           flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+@app.route("/account")
+@login_required
+def account():
+    form=UpdateAccountForm()
+    image_file = url_for('static',filename='profile_pics/'+ current_user.image_file)
+    return render_template('account.html', title='account',
+                           image_file=image_file, form=form)
 
