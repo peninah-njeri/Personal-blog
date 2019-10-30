dde2a2c38565e88c6558f5a65824ca90128529b7
peninah-njeri <peninahgathuru@gmail.com>
2019-10-30 10:59:52 +0300

    implement pagination to help dispay recently posted posts

--- app/routes.py
+++ app/routes.py
@@ -1,30 +1,18 @@
 import os
-from flask import render_template,url_for,flash,redirect
+from flask import render_template,url_for,flash,redirect,abort
 import secrets
-from app.forms import RegistrationForm, LoginForm,UpdateAccountForm
+from app.forms import RegistrationForm, LoginForm,UpdateAccountForm, PostForm
 from app import app,db,bcrypt
-from app.models import User
+from app.models import User,Post
 from flask_login import login_user, current_user, logout_user,login_required
 
-posts = [
-    {
-        'author': 'Naomi Watts',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 20, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 21, 2018'
-    }
-]
+
 
 @app.route('/')
 @app.route('/home')
 def home():
-   return render_template('home.html',posts=posts)
+    posts =Post.query.order_by(Post.date_posted())
+    return render_template('home.html',posts=posts)
 
 @app.route('/about')
 def about():
@@ -37,7 +25,7 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password= bcrypt.generate_password_hash(form.password.data).decode ("__utf-8")
-        user= User(username=form.username.data, email=form.email.data, password=hashed_password)
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
         db.session.add(user)
         db.session.commit()
         flash('Your account has been created! You are now able to login', 'success')
@@ -94,7 +82,57 @@ def account():
     return render_template('account.html', title='account',
                            image_file=image_file, form=form)
 
-@app.route("/post/new")
+@app.route("/post/new", methods=['GET', 'POST'])
 @login_required
 def new_post():
-    return render_template('create_post.html', title='New Post')
+    form = PostForm()
+    if form.validate_on_submit():
+
+        post = Post(title=form.title.data, content =form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title='New Post',
+                           form=form,legend='New Post')
+
+
+
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
+
+
+@app.route("/post/<int:post_id>/update, methods=['GET', 'POST']")
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title =form.title.data
+        post.content=form.content.data
+        db.session.commit()
+        flash('Your post has been updated!','success')
+        return redirect(url_for('post', post_id=post.id))
+
+    form.title.date =post.title
+    form.content.data =post.content
+    return render_template('create_post.html', title='Update Post',
+                           form=form, legend='Update Post')
+
+
+
+
+@app.route("/post/<int:post_id>/delete, methods=['POST']")
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!','success')
+    return redirect(url_for('home'))
